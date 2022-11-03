4a8803b2c1dfec845c91043cd800d01f33667947
yellow <yellow@wordncode.com>
2022-11-03 18:04:55 +0900

    메인 작업중

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
+    videoArea();
     lookServiceSwiper();
     
 
 });
+function videoArea() {
+  var mainswiper = new Swiper(".video-area__inner.swiper-container", {
+  }); 
+}
 function lookServiceSwiper() {
     var mainswiper = new Swiper(".look-service__inner.swiper-container", {
       pagination: {
